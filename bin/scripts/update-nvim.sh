77b10264d6d9198b9899ec37d1e6932a45ae3585
Marlrus <36910211+Marlrus@users.noreply.github.com>
2021-10-20 20:33:41 -0500

    Removed hardcoded checksum for update-nvim, using checksum from webpage through curl

--- bin/scripts/update-nvim.sh
+++ bin/scripts/update-nvim.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-CHECKSUM="fddd4a83875a0b03ad163a63a2f80cf908cccb2fff640e1210c7f115560d3cc4"
-
 function err_exit(){
   echo $1
   exit 1
@@ -16,9 +14,9 @@ echo "Nvim latest dowloaded to home directory." ||
 err_exit "Error dowloading NVIM"
 
 if test -f ~/nvim.appimage; then
-  echo "Checking checksum"
+  echo "Verifying checksum"
   # In case checksum changes
-  # CHECKSUM=$(curl https://github.com/neovim/neovim/releases/tag/nightly | rg SHA256 -A 3| rg ".*nvim.appimage$" | awk '{print $1}')
+  CHECKSUM=$(curl https://github.com/neovim/neovim/releases/tag/nightly | rg SHA256 -A 3| rg ".*nvim.appimage$" | awk '{print $1}')
   if [[ $(sha256sum ~/nvim.appimage | awk '{print $1}') != $CHECKSUM ]]; then
     err_exit "Checksum not matching"
   else
