8ae593fd20f6e539e0257b62d81942d85dcec533
Marlrus <36910211+Marlrus@users.noreply.github.com>
2021-10-23 20:59:28 -0500

    Inquirer testing and example added

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,7 @@ import updateNvim from "./commands/update-nvim.js";
 import updateSystem from "./commands/update-system.js";
 // import test from "./commands/test.js";
 import nvmInstall from "./commands/nvm-install.js";
+// import inquirerTest from "./commands/inquirer-test.js";
 
 const program = new Command();
 
@@ -12,6 +13,7 @@ program.addCommand(updateNvim());
 program.addCommand(updateSystem());
 // program.addCommand(test());
 program.addCommand(nvmInstall());
+// program.addCommand(inquirerTest());
 // Command for updating vim "vim +PlugInstall +qall"
 // Command for installing all lsps in npm "nvm reinstall-packages <version>" use awk and nvm list to get default version
 
